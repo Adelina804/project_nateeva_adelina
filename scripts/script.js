f01afc28de808fbf5c4607e4305574881951d97c
Adelina804 <adelina.nateeva.@gmail.com>
2025-05-27 15:34:39 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -27,3 +27,41 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     }
 });
+
+// 2. Динамический вывод навигационного меню
+const headerMenu = document.querySelector('.header__menu');
+if (headerMenu){
+          const headerList = headerMenu.querySelector('.header__list');
+          const menuData = {
+            link1: {
+                link: '#',
+                title: 'Главная',
+            },
+            link2: {
+                link: '#',
+                title: 'Словарик музеолога',
+            },
+            link3: {
+                link: '#',
+                title: 'Полезные источники',
+            },
+            link4: {
+                link: '#',
+                title: 'Музеи г. Чистополь',
+            }
+        }
+const createLink = (UrlLink, title) =>{
+     const link = `
+            <li class="header__item"><a href="${UrlLink}" class="header__link">${title}</a></li>
+            `;
+            return link;
+        }
+for (const linkItem in menuData) {
+  const link = menuData[linkItem];
+  const linkIndex = createLink(link.UrlLink, link.title);
+  headerList.insertAdjacentHTML('beforeend', linkIndex);
+
+  }
+}
+
+console.log('Навигацинное меню создано с помощью javascript!');
